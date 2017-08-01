54e35bc79e2035370b4301399c5ee1d5a80c7327
tobi.dara <tobi_daramola@yahoo.com>
2017-08-01 12:30:42 +0100

    increase number of test cases for valid and unvalid input

--- tests/test.js
+++ tests/test.js
@@ -7,7 +7,7 @@ describe("AritGeo", function() {
         it("should return Arithmetic for [2,4,6,8,10,12,14]", function() {
             assert.equal(myApp.aritGeo([2, 4, 6, 8, 10, 12, 14]), 'Arithmetic');
         });
-        it("should return Geomtric for [2,4,8,16,32,64]", function() {
+        it("should return Geomtric for [5,10,20,40,80,160]", function() {
             assert.equal(myApp.aritGeo([2, 4, 8, 16, 32, 64]), 'Geometric');
         });
         it("should return -1 for [3,7,4,8,9,10,12]", function() {
@@ -18,11 +18,23 @@ describe("AritGeo", function() {
         });
     })
     describe("handle invalid input", function() {
-        it("should return undefined as aritGeo for [a,b]", function() {
-            assert.equal(myApp.aritGeo([a, b]), 'undefined');
+        it("should return not a sequence for [4]", function() {
+            assert.equal(myApp.aritGeo([4]), 'not a sequence');
         });
-        it("should return all element should be numbers as aritGeo for [12,a,b,13,4]", function() {
-            assert.equal(myApp.aritGeo(), 'numbers only');
+        it("should return all element should be numbers  for [12,'a','b',13,4]", function() {
+            assert.equal(myApp.aritGeo([12, 'a', 'b', 13, 4]), 'numbers only');
+        });
+        it("should return all element should be numbers  for ['a']", function() {
+            assert.equal(myApp.aritGeo(['a']), 'numbers only');
+        });
+        it("should return all element should be numbers  for [2, 4, 6, 8, 10, 'peter']", function() {
+            assert.equal(myApp.aritGeo([2, 4, 6, 8, 10, 'peter']), 'numbers only');
+        });
+        it("should return all element should be numbers  for [0]", function() {
+            assert.equal(myApp.aritGeo([0]), 'array[0] != 0 in geometric sequence');
+        });
+        it("should return all element should be numbers  for [0]", function() {
+            assert.equal(myApp.aritGeo(({ boy: 24 })), 'not an array');
         });
     })
 })
